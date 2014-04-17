fa26fc0df991af54b98bd1b57ef8e4e4efdc022e
ZINGER JAMES J <zngj0025@l209-n-018.mediastudies.ca>
2014-04-16 21:25:28 -0400

    Game model inital draft complete
    should work but needs testing

---
+++ Axon2048/Axon2048/Cardaction.m
@@ -0,0 +1,13 @@
+//
+//  CardAction.m
+//  Axon2048
+//
+//  Created by ZINGER JAMES J. on 2014-04-16.
+//  Copyright (c) 2014 Axon Interactive. All rights reserved.
+//
+
+#import "CardAction.h"
+
+@implementation CardAction
+
+@end
---
+++ Axon2048/CardAction.h
@@ -0,0 +1,19 @@
+//
+//  CardAction.h
+//  Axon2048
+//
+//  Created by ZINGER JAMES J. on 2014-04-16.
+//  Copyright (c) 2014 Axon Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TileIndex.h"
+
+@interface CardAction : NSObject
+
+@property TileIndex     lookupIndex;
+@property TileIndex     newIndex;
+@property int           newValue;
+@property BOOL          shouldDelete;
+
+@end
